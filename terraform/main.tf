12ed6f436988d5492059688da314526945239422
Артем Малов <admalov>
2023-02-01 00:45:05 +0300

    terraform for function

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,46 +11,20 @@ provider "yandex" {
   zone = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8irfoscugmpsoanefh"
+resource "yandex_function" "get-token" {
+    name               = "get-token"
+    description        = "change yandex code to oauth token"
+    user_hash          = "get-token-0.0.1"
+    runtime            = "nodejs16"
+    entrypoint         = "index"
+    memory             = "128"
+    execution_timeout  = "10"
+    service_account_id = "ajefhv2s2lhfrgadpcmj"
+    content {
+        zip_filename = "../functions/dist/get-token.zip"
     }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
 }
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "yandex_function_get-token-function" {
+    value = "${yandex_function.get-token.id}"
 }
